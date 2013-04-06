b4a151c2b87685f2fa4395ef15f992adb1138ef6
Valder Gallo <valdergallo@gmail.com>
2013-04-06 02:32:26 -0300

    Remove sphinxtogithub

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinxtogithub',
+    # 'sphinxtogithub',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
