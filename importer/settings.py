8b258610a591938bc66a1eee41d2afbdaa358081
Valder Gallo <valdergallo@gmail.com>
2013-07-04 17:56:07 -0300

    Fixed settings for all djangos

--- importer/settings.py
+++ importer/settings.py
@@ -137,23 +137,22 @@ INSTALLED_APPS = (
     'importer'
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+
+def skip_unreadable_post():
+    return True
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
+        'special': {
+            '()': skip_unreadable_post,
         }
     },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
-            'filters': ['require_debug_false'],
+            'filters': ['special'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
@@ -165,6 +164,7 @@ LOGGING = {
         },
     }
 }
+
 
 # Local settings
 try:
