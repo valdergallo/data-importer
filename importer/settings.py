94aac8903b060a125ad4ef33c45fd5849ddd9a76
Valder Gallo <valdergallo@gmail.com>
2013-07-04 17:57:15 -0300

    Remove static files for django 1.2

--- importer/settings.py
+++ importer/settings.py
@@ -83,8 +83,8 @@ STATICFILES_DIRS = (
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # 'django.contrib.staticfiles.finders.FileSystemFinder',
+    # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
