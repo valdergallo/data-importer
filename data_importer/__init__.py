4a0b04987bf583b5a9e602e0607f7047e6ae2870
Valder Gallo <valdergallo@gmail.com>
2013-10-03 23:10:37 -0300

    fixed init

--- data_importer/__init__.py
+++ data_importer/__init__.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 __doc__ = 'Data Importer'
 __version__ = '1.2.1'
 __author__ = 'Valder Gallo <valdergallo@gmail.com>'
 
-
-from .importers.base import BaseImporter
 from .importers.xml_importer import XMLImporter
 from .importers.xls_importer import XLSImporter
 from .importers.xlsx_importer import XLSXImporter
