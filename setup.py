f6906a9bb62185a61404e2cc84ef99564f2bb2de
Valder Gallo <valdergallo@gmail.com>
2014-11-02 11:39:09 -0200

    Fix get long description

--- setup.py
+++ setup.py
@@ -8,22 +8,30 @@ from setuptools import setup
 import data_importer
 
 
+def readme():
+    try:
+        with open('README.rst') as f:
+            return f.read()
+    except:
+        return '''**Django Data Importer** is a tool which allow you to transform easily a CSV, XML, XLS and XLSX file into a python object or a django model instance. It is based on the django-style declarative model.'''
+
+
 install_requires = [
-    'django>=1.2',
-    'openpyxl==1.6.1',
-    'xlrd==0.9.0',
+    'django>=1.4',
+    'openpyxl==2.1.2',
+    'xlrd==0.9.3',
 ]
 
 
 setup(name='data-importer',
       url='https://github.com/valdergallo/data-importer',
+      download_url='https://github.com/valdergallo/data-importer/tarball/v%s/' % data_importer.__version__,
       author="valdergallo",
       author_email='valdergallo@gmail.com',
       keywords='Data Importer XLS XLSX CSV XML',
       description='Simple library to easily import data with Django',
       license='BSD',
-      long_description=('''**Django Data Importer** is a tool which allow you to transform easily a CSV, XML, XLS and XLSX file
-      into a python object or a django model instance. It is based on the django-style declarative model.'''),
+      long_description=readme(),
       classifiers=[
           'Framework :: Django',
           'Operating System :: OS Independent',
