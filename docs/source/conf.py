5ae7b4332c57c8871f36befd760efab0a40f7292
Valder Gallo <valdergallo@gmail.com>
2014-11-09 17:54:14 -0200

    Fix read docs template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,7 +107,7 @@ pygments_style = 'sphinx'
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # if not on_rtd:  # only import and set the theme if we're building docs locally
-if on_rtd:  # only import and set the theme if we're building docs locally
+if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
