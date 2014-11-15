220f3d2fd8e3421b5fa9393c018d1e12750d90df
Valder Gallo <valdergallo@gmail.com>
2014-11-15 13:49:51 -0200

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -108,9 +108,9 @@ on_rtd = os.environ.get('READTHEDOCS', False)
 
 # if not on_rtd:  # only import and set the theme if we're building docs locally
 if on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
+    #import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 else:
     html_theme = 'default'
 
